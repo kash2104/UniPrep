cf76eca6d0de77a3fb938b9cd5db53f1e10c5baa
kash2104 <kparikh1104@gmail.com>
2024-05-03 09:14:30 +0530

    API and apiconnector

---
+++ src/services/apis.js
@@ -0,0 +1,22 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+//AUTH APIS
+export const authEndpoints = {
+    SENDOTP_API: BASE_URL + '/auth/sendotp',
+    SIGNUP_API: BASE_URL + '/auth/signup',
+    LOGIN_API: BASE_URL + '/auth/login',
+    CHANGEPASSWORD_API: BASE_URL + '/auth/changePassword',
+}
+
+//COURSE APIS
+export const courseEndpoints = {
+    ADDCOURSE_API: BASE_URL + '/course/addCourse'
+}
+
+//EXAM PAPER APIS
+export const examPaperEndpoints = {
+    UPLOAD_PAPER_API: BASE_URL + '/examPaper/uploadPaper',
+    COURSE_PAPER_API: BASE_URL + '/examPaper/getCoursePaper',
+    ALL_PAPER_API: BASE_URL + '/examPaper/getAllPapers',
+    DELETE_PAPER_API: BASE_URL + '/examPaper/deletePaper'
+}
