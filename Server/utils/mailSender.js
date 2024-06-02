bec02bd935d632d207852309a38091941e7727e4
kash2104 <kparikh1104@gmail.com>
2024-06-02 15:52:41 +0530

    UniShare to UniPrep changed.

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -1,27 +1,26 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const mailSender = async(email, title, body) => {
-    try {
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth:{
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-        })
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
-        let info = await transporter.sendMail({
-            from: 'UniShare',
-            to: `${email}`,
-            subject: `${title}`,
-            html: `${body}`
-        })
-        console.log('mail information: ', info);
-        return info;
-    }
-    catch (error) {
-        console.log('Error while sending mail: ', error);    
-    }
-}
+    let info = await transporter.sendMail({
+      from: "UniPrep",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+    console.log("mail information: ", info);
+    return info;
+  } catch (error) {
+    console.log("Error while sending mail: ", error);
+  }
+};
 
 module.exports = mailSender;
