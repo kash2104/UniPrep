d83a6ebf985bd028beab4c7f7ba704b511341e7f
kash2104 <kparikh1104@gmail.com>
2024-06-02 14:29:05 +0530

    Changed from UniShare to UniPrep

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Template from "../components/core/Auth/Template";
 
 function Signup() {
-  return <Template text="Welcome to UniShare" formType="signup" />;
+  return <Template text="Welcome to UniPrep" formType="signup" />;
 }
 
 export default Signup;
