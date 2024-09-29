694040f5b384422f5561b934ae0fbd90d8167981
kash2104 <kparikh1104@gmail.com>
2024-09-29 07:33:03 +0530

    route token protected

--- src/components/core/Auth/PrivateRoute.jsx
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const token = useSelector((state) => state.auth);
+  const token = useSelector((state) => state.auth.token);
 
   if (token !== null) {
     return children;
